c983e91e6fcc8174d92fa040c10bc2a558e58220
fanwenl <fanwenl@Foxmail.com>
2018-08-20 18:17:16 +0800

    update file

--- hello_world/README.md
+++ hello_world/README.md
@@ -2,4 +2,6 @@
 
 Starts a FreeRTOS task to print "Hello World"
 
-See the README.md file in the upper level 'examples' directory for more information about examples.
+在`app_main()`函数中分别为两个CPU各创建一个hello world程序
+
+* 两个任务的优先级可以相同。
--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -9,32 +9,65 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_log.h"
 
+#define ESP_TASK_APP0_PRIO ESP_TASK_PRIO_MIN+3
+#define ESP_TASK_APP1_PRIO ESP_TASK_PRIO_MIN+3
+
+static const char* TAG = "HELLO";
+
+void app_cpu1(void *arg);
+void app_cpu0(void *arg);
 
 void app_main()
 {
-    printf("Hello world!\n");
-
+    BaseType_t res;
     /* Print chip information */
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
     printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
 
     printf("silicon revision %d, ", chip_info.revision);
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
+    res = xTaskCreatePinnedToCore(&app_cpu0, "app_cpu0",
+                                  2048, NULL,
+                                  ESP_TASK_APP0_PRIO, NULL, 0);
+    assert(res == pdTRUE);
+    ESP_LOGI(TAG, "Starting task on CPU0.");
+
+    res = xTaskCreatePinnedToCore(&app_cpu1, "app_cpu1",
+                                  2048, NULL,
+                                  ESP_TASK_APP1_PRIO, NULL, 1);
+    assert(res == pdTRUE);
+    ESP_LOGI(TAG, "Starting task on CPU1.");
+}
+
+void app_cpu1(void *arg)
+{
+    ESP_LOGI(TAG, "Hello world!->CoreID:%d", xPortGetCoreID());
+
+    while (1)
+    {
+        ESP_LOGI(TAG,"Hello CPU1->CoreID:%d", xPortGetCoreID());
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+}
+void app_cpu0(void *arg)
+{
+    ESP_LOGI(TAG,"Hello world!->CoreID:%d", xPortGetCoreID());
+
+    while (1)
+    {
+        ESP_LOGI(TAG,"Hello CPU0->CoreID:%d", xPortGetCoreID());
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
